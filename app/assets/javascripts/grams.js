daf4f6f76bf5593d58189e8c27317e61466c1792
adamjgoodman <goodmanadamj@gmail.com>
2020-02-10 22:26:25 -0500

    sped up masonry loading

--- app/assets/javascripts/grams.js
+++ app/assets/javascripts/grams.js
@@ -2,7 +2,7 @@
 $(document).on('turbolinks:load', function() {
   var container = document.querySelector('.grid');
   // initialize Masonry after all images have loaded
-  imagesLoaded( '.grid-item', function() {
+  imagesLoaded( 'html', function() {
       var msnry = new Masonry( container, {
           itemSelector: '.grid-item',
           columnWidth: 20,
